13ade09b8d1a119f62a5f696a5afa3c4cfd9bf31
tranthudat2k1 <thudatdl123@gmail.com>
2023-02-25 20:28:07 +0700

    change password success

--- server/src/routes/authentication.router.js
+++ server/src/routes/authentication.router.js
@@ -1,6 +1,9 @@
 const authController = require('../controllers/authentication.controller')
+const { verifyToken } = require('../middlewares')
 
 module.exports = require('express').Router()
   .post("/login", authController.login)
   .post("/register", authController.create)
+  .put('/logout', verifyToken ,authController.logOut)
   .get("/all", authController.getAll)
+  .put("/change-password",verifyToken ,authController.changePasswordController)
