e682177f8c9cf3355b40a8a2a8c0900b4330b31f
TanNgoc <factyel.bttn@gmail.com>
2023-03-01 19:46:17 +0700

    add sendmail service, an test api in authentication.router, edit .env

--- server/src/routes/authentication.router.js
+++ server/src/routes/authentication.router.js
@@ -1,6 +1,13 @@
 const authController = require('../controllers/authentication.controller')
+const { SendMail, SendMailText } = require('../services/sendmail.service')
 
 module.exports = require('express').Router()
   .post("/login", authController.login)
   .post("/register", authController.create)
   .get("/all", authController.getAll)
+  .get("/mail", (req, res) => {
+    SendMailText("factyel.bttn@gmail.com", "test mail from job", "xin chao")
+      .then((rel) => res.send(rel))
+      .catch(err => res.send(err))
+
+  })
